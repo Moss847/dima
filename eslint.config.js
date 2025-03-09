7b9d1da11734553dacd14d3bc5895d0ac096f747
Kursovaya <mosxud.bli777@gmail.com>
2025-03-09 12:46:11 +0500

    Новое

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,34 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import tseslint from '@typescript-eslint/eslint-plugin';
 import pluginReact from 'eslint-plugin-react';
+import pluginPrettier from 'eslint-plugin-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { languageOptions: { globals: globals.browser } },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: {
+      globals: globals.browser,
+      parser: '@typescript-eslint/parser', // Используем парсер TypeScript
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true, // Включаем поддержку JSX
+        },
+      },
+    },
+    plugins: {
+      react: pluginReact,
+      prettier: pluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': 'error', // Ошибка при несоответствии Prettier
+      'react/react-in-jsx-scope': 'off', // Не требуется в React 17+
+      // Добавьте другие правила по вашему усмотрению
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Button } from '@mantine/core';
 import { useDispatch, useSelector } from 'react-redux';
 import { increment, decrement } from './store/exampleSlice';
-import { RootState } from './store/rootReducer'
+import { RootState } from './store/rootReducer';
 import { useTranslation } from 'react-i18next';
 
 function App() {
   const dispatch = useDispatch();
   const value = useSelector((state: RootState) => state.example.value);
-const { t, i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
 
   const changeLanguage = (lng: string) => {
     i18n.changeLanguage(lng);
